fb6bbd1033e23598a3ae805bd0cd880246f0a252
Matan <mtmt8520@gmail.com>
2025-09-08 19:34:32 +0300

    flusk

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def main():
     return app.send_static_file('main/main.html')
 
+@app.route('/login', methods=['GET'])
+def main():
+    return app.send_static_file('login/login.html')
+
+@app.route('/register', methods=['GET'])
+def main():
+    return app.send_static_file('register/register.html')
+
+@app.route('/dashboard', methods=['GET'])
+def main():
+    return app.send_static_file('dashboard/dashboard.html')
+
 @app.route('/<filename>', methods=['GET'])
 def static_files(filename):
     return app.send_static_file(filename)
