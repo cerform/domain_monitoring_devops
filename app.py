938d56d14fd581dfc634b8ccdc98e73ac5727b6d
Matan <mtmt8520@gmail.com>
2025-09-08 20:55:47 +0300

    flusk app improvments

--- app.py
+++ app.py
@@ -1,34 +1,40 @@
 from flask import Flask,request,jsonify
+import UserManagementModule
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def main():
+def main_page():
     return app.send_static_file('main/main.html')
 
 
 @app.route('/login', methods=['GET','POST'])
-def main():
+def login():
     if request.method == 'GET':
         return app.send_static_file('login/login.html')
     else:
         loginInfo=request.json
         ## check login details from json
-
+        pass
 
 @app.route('/register', methods=['GET','POST'])
-def main():
+def register():
     if request.method == 'GET':
         return app.send_static_file('register/register.html')
     else:
         registerInfo=request.json
         ## check register details from json
-
+        pass
 
 @app.route('/dashboard', methods=['GET'])
-def main():
+def dashboard():
     return app.send_static_file('dashboard/dashboard.html')
 
+@app.route('/add_domain', methods=['POST'])
+def add_domain():
+    domain_info = request.json
+    ## process domain_info
+    pass
 
 @app.route('/<filename>', methods=['GET'])
 def static_files(filename):
