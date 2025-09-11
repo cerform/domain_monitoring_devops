6d59290ca969e4b964bfdfcf6892352e2f6238f9
Assaf <assaf.naaman.work@gmail.com>
2025-09-11 19:50:48 +0300

    register page: Assaf: html css and javascript file mostly completes.

---
+++ static/register/register.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page is loaded!')
+    // Define all constants at the beginning
+    const Username = document.querySelector('#username');
+    const Password = document.querySelector('#password');
+    const PasswordConfirmation = document.querySelector('#password_confirmation');
+    const ErrorMessage = document.querySelector('#error-message');
+    const SuccessMessage = document.querySelector('#error-message');
+    const Form = document.querySelector('form');
+    
+    // Form submission event listener
+    Form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        // reset messages
+        SuccessMessage.style.display = 'none'
+        ErrorMessage.style.display = 'none'
+
+        // Display a message
+        let name = Username.value;
+        let pass = Password.value;
+        let pass_conf = PasswordConfirmation.value;
+        registerUser(name, pass, pass_conf);
+
+        // Reset the form after submission
+        event.target.reset();
+    });
+
+    // Function to get weather information
+    async function registerUser(name, pass, pass_conf) {
+        content = {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: name,
+                password: pass,
+                password_confirmation: pass_conf
+            })
+        };
+        const response = await fetch(`/regsiter`, content);
+        const data = await response.json();
+        if (data[0] == "FAILED"){
+            ErrorMessage.textContent = `${data[1]}`
+            ErrorMessage.style.display = 'block'
+        }
+        else {
+            SuccessMessage.textContent = 'User Registration Succeeded!'
+            SuccessMessage.style.display = 'block'
+            setTimeout(function() { window.location.href = "/login"; }, 2000); 
+        }
+    }
+});
