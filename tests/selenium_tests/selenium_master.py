e73a174e19abd3186edf5b2fe3932aa297c9ec25
Matan <mtmt8520@gmail.com>
2025-11-13 19:06:58 +0200

    add files for tests

--- tests/selenium_tests/selenium_master.py
+++ tests/selenium_tests/selenium_master.py
@@ -6,7 +6,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
+import selenium_register
 import selenium_login
+import selenium_single_domain
+import selenium_bulk_domain
 
 def create_linux_driver():
     options = Options()
@@ -29,7 +32,14 @@ if __name__ == "__main__":
     driver = create_linux_driver()
     # Connect to the web application
     driver.get("http://127.0.0.1:8080/")
-    # Wait for the page to load
-    selenium_login.login(driver, "admin", "admin")
+    # Test registration functionality
+    selenium_register.register(driver)
+    # Test login functionality
+    selenium_login.login(driver)
+    # Test add single domain functionality
+    selenium_single_domain.add_single_domain(driver)
+    # Test add bulk domains functionality
+    selenium_bulk_domain.add_bulk_domains(driver)
+
     time.sleep(10)
     driver.quit()
