4505748a19e246790a37ce0ebd230769ec8525d0
imse <im.se@outlook.com>
2025-11-13 18:58:40 +0200

    import changed

--- tests/api_tests/Aux_Library.py
+++ tests/api_tests/Aux_Library.py
@@ -7,8 +7,8 @@ from UserManagementModule import UserManager as UM
 # -----------------------------------------------------
 # Global session and Base URL configuration
 # -----------------------------------------------------
-BASE_URL = os.getenv("BASE_URL", "http://import tests.api_tests.Aux_Library as Aux_Library:8080")
-session = requests.Session()
+BASE_URL = "http://localhost:8080"
+
 
 
 # -----------------------------------------------------
