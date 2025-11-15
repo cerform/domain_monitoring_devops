6ee0a43b01e432d582282577ed7e2fddf5545ecf
Ozou87 <oefraty@gmail.com>
2025-11-15 16:19:01 +0200

    selenium func

--- tests/selenium_tests/selenium_add_single_domain.py
+++ tests/selenium_tests/selenium_add_single_domain.py
@@ -1,9 +1,26 @@
-import time
-import pytest
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+import time
+import pytest
+
+BASE_URL = "http://localhost:8080"
+
+@pytest.fixture
+def driver():
+    """
+    Creating a Chrome WebDriver  instance
+    """
+    options = options()
+
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(5)
+
+    yield driver
+    driver.quit()
+    
+
 
