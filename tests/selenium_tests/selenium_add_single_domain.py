829cd24200065d0b0e66732895cdc3689f1b0c07
Ozou87 <oefraty@gmail.com>
2025-11-15 17:13:57 +0200

    add_single_dom_test_sel

--- tests/selenium_tests/selenium_add_single_domain.py
+++ tests/selenium_tests/selenium_add_single_domain.py
@@ -12,7 +12,7 @@ BASE_URL = "http://localhost:8080"
 @pytest.fixture
 def driver():
     """
-    Creating a Chrome WebDriver  instance
+    Creating a Chrome WebDriver instance
     """
     options = options()
 
@@ -21,6 +21,34 @@ def driver():
 
     yield driver
     driver.quit()
-    
+
+def login(driver, username: str, password: str):
+    """
+    Logging in through the UI so we can reach the Dashboard
+    """
+    driver.get(f"{BASE_URL}/login")
+
+    user_input = driver.find_element(By.ID, "username")
+    pass_input = driver.find_element(By.ID, "password")
+    login_submit = driver.find_element(By.CSS_SELECTOR, "input[type='submit']")
+
+    user_input.clear()
+    user_input.send_keys(username)
+
+    pass_input.clear()
+    pass_input.send_keys(password)
+
+    login_submit.click()
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "openAddDomain"))
+                                    )
+
+def add_single_domain_and_result(driver):
+    pass
+
+
+
+
 
 
