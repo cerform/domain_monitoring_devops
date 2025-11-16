844bfaa23a8233c2c9843e699597d76bd08f09f6
Assaf <assaf.naaman.work@gmail.com>
2025-11-16 20:23:51 +0200

    added load function to load page in the driver on dashboard_page.py.

--- tests/selenium_tests/pages/dashboard_page.py
+++ tests/selenium_tests/pages/dashboard_page.py
@@ -2,16 +2,21 @@ from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 from pages.bulk_upload_modal import BulkUploadModal
 from pages.single_domain_modal import SingleDomainModal
+from tests.api_tests.Aux_Library import BASE_URL
 
 
 
 class DashboardPage(BasePage):
-
+    URL = f"{BASE_URL}/dashboard"
+    # Locators:
     welcome_message = (By.ID, "greeting")
     add_domain_button = (By.ID, "openAddDomain")
     bulk_upload_button = (By.ID, "openBulkUpload")
     logout_button = (By.ID, "logoutBtn")
     scan_now_button = (By.ID, "scanNowBtn")
+    # Actions:
+    def load(self):
+        self.driver.get(self.URL)    
 
     def get_welcome_message(self):
         return self.get_text(locator=self.welcome_message)
