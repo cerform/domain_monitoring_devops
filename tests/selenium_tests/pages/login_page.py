6cbbdf8ef38b1fff6d50ad742f588654c54f1468
Matan <mtmt8520@gmail.com>
2025-11-16 19:24:50 +0200

    new login tests

---
+++ tests/selenium_tests/pages/login_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from base_page import BasePage
+
+class LoginPage(BasePage):
+    # Locators:
+    username_input = (By.ID, "username")
+    password_input = (By.ID, "password")
+    login_button = (By.CSS_SELECTOR, "input[type='submit'][value='Login']")
+    register_button = (By.ID, "register")
+    # Actions:
+    def enter_username(self, username):
+        self.type(self.username_input, username)
+
+    def enter_password(self, password):
+        self.type(self.password_input, password)
+
+    def click_login(self):
+        self.click(self.login_button)
+
+    def login(self, username, password):
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login()
+    
+    def move_to_login_page(self):
+        self.click(self.register_button)
